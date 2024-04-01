6427c25c33d98d16555f59eb01c962824055ad56
Kevin-Jimenez-D <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2024-04-01 13:41:28 -0500

    feat: :construction: I return the data but I have problems with save
    The mistakes are with the names of the tables

--- src/main/resources/sql/filter.sql
+++ src/main/resources/sql/filter.sql
@@ -1,23 +1,23 @@
 DROP DATABASE IF EXISTS filtro;
 CREATE DATABASE filtro;
 USE filtro; 
-CREATE TABLE TypeOfContent(
-    id VARCHAR(255) NOT NULL,
+CREATE TABLE type_of_content(
+    id VARCHAR(255) NOT NULL ,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE StateVisualization(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     stateVisualization VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE Gender(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     gender VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE Costumer(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     firstName VARCHAR(255) NOT NULL,
     secondName VARCHAR(255) NULL,
     firstLastName VARCHAR(255) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE Costumer(
     PRIMARY KEY(id)
 );
 CREATE TABLE Platform(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     name VARCHAR(255) NOT NULL,
     typeOfContent_ID VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
@@ -36,14 +36,14 @@ CREATE TABLE CostumerAudioVisual(
     PRIMARY KEY(costumer_ID,audioVisual_ID)
 );
 CREATE TABLE Register(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     qualification DOUBLE NOT NULL,
     comment VARCHAR(255) NOT NULL,
     audioVisual_ID VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE AudioVisual(
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) NOT NULL ,
     name VARCHAR(255) NOT NULL,
     platform_ID VARCHAR(255) NOT NULL,
     gender_ID VARCHAR(255) NOT NULL,
@@ -58,7 +58,7 @@ ALTER TABLE
 ALTER TABLE
     AudioVisual ADD CONSTRAINT audiovisual_statevisualization_id_foreign FOREIGN KEY(stateVisualization_ID) REFERENCES StateVisualization(id);
 ALTER TABLE
-    Platform ADD CONSTRAINT platform_typeofcontent_id_foreign FOREIGN KEY(typeOfContent_ID) REFERENCES TypeOfContent(id);
+    Platform ADD CONSTRAINT platform_typeofcontent_id_foreign FOREIGN KEY(typeOfContent_ID) REFERENCES type_of_content(id);
 ALTER TABLE
     CostumerAudioVisual ADD CONSTRAINT costumeraudiovisual_audiovisual_id_foreign FOREIGN KEY(audioVisual_ID) REFERENCES AudioVisual(id);
 ALTER TABLE
